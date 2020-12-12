eeed07333b06f8cd48de90fe1b077acb7b672f22
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-11 16:51:54 -0800

    the test for absolute function from lib.js is tested

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,5 +1,16 @@
+const lib = require('../lib');
 
-test('Our first test', ()=>{
+test('absolute - should return positive if number is positive', ()=>{
+  const result = lib.absolute(1);
+  expect(result).toBe(1);
+});
 
-  
+test('absolute - should return positive if number is negative', ()=>{
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
+
+test('absolute - should return 0 if number is zero', ()=>{
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
 });
