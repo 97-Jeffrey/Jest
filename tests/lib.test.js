e12ef0e9b8ce6f778d13f073052832cd2187b88e
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-11 16:57:07 -0800

    refractor the test with describe it block

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,16 +1,19 @@
 const lib = require('../lib');
 
-test('absolute - should return positive if number is positive', ()=>{
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
 
-test('absolute - should return positive if number is negative', ()=>{
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
-
-test('absolute - should return 0 if number is zero', ()=>{
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
-});
+describe('absolute function', ()=>{
+  it('should return positive if number is positive', ()=>{
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+  
+  it('should return positive if number is negative', ()=>{
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+  
+  it('should return 0 if number is zero', ()=>{
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
+})
