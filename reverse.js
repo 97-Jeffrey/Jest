58c4ad214940c4d703b4c2f850c6560acc313b82
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-15 15:07:24 -0800

    reverseVowel function is added and tested

--- reverse.js
+++ reverse.js
@@ -1,9 +1,7 @@
 module.exports.reverseOnlyLetters = function(S) {
     
   let letters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  
   let arr = S.split('');
-  
   let left = 0, right = arr.length-1;
   
   while(left<right){
@@ -24,5 +22,31 @@ module.exports.reverseOnlyLetters = function(S) {
   
   return arr.join('');
   
+};
+
+
+module.exports.reverseVowels = function(s) {
   
+    let S = s.split("");
+    let vowels ='aeiouAEIOU';
+    let left =0;
+    let right =S.length-1;
+
+    while(left<right){
+        
+        if(left<right &&!vowels.includes(S[left])){
+            left++;
+        }
+        if(left<right && !vowels.includes(S[right])){
+            right--;
+        }
+        else{
+            let temp = S[left];
+            S[left] = S[right];
+            S[right]=temp;
+            left++;
+            right--;    
+        }
+    }
+    return S.join('');
 };
--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -12,3 +12,15 @@ describe('the function reverseOnlyLetter', ()=>{
     expect(result3).toBe("Qedo1ct-eeLg=ntse-T!");
   })
 })
+
+describe('the function reverseVowels', ()=>{
+
+  it('should only reverse position of vowels', ()=>{
+
+    const result1 = reverse.reverseVowels("hello");
+    const result2 = reverse.reverseVowels("leetcode");
+
+    expect(result1).toBe("holle");
+    expect(result2).toBe('leotcede');
+  })
+})
